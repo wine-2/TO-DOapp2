944723baee22f8f03c48fe09f6506fbadeadddd0
ESPORTS-137\cre <t-shirai@oca.ac.jp>
2022-10-18 19:09:44 +0900

    後半の作業

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,3 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/list', [TodoListController::class, 'index']);
+Route::resource('tasks', TaskController::class);
